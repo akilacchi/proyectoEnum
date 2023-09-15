71e5d2e789a2ae46e8b427e16708498d846d0ad2
xGaby <gabrielamar26@yahoo.es>
2023-09-14 20:13:47 -0500

    Modificacion entidad usuario: atributos cliente

--- src/main/java/com/equipoa/servicewebapp/Entidades/Usuario.java
+++ src/main/java/com/equipoa/servicewebapp/Entidades/Usuario.java
@@ -4,9 +4,11 @@ import com.equipoa.servicewebapp.Enum.Provincias;
 import com.equipoa.servicewebapp.Enum.Rol;
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ID;
@@ -20,6 +22,24 @@ public class Usuario {
     @Enumerated(EnumType.STRING)
     private Provincias provincia;
 
+    //atributos para cliente
+    private String direccion;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Trabajo> trabajosCliente;
+
+    @OneToMany(mappedBy = "proveedor")
+    private List<Trabajo> trabajosProveedor;
+
+    @OneToMany(mappedBy = "clienteEmisor")
+    private List<Calificacion> calificacionesEmitidas;
+
+    @OneToMany(mappedBy = "proveedorReceptor")
+    private List<Calificacion> calificacionesRecibidas;
+
+    public Usuario() {
+    }
+
     public Usuario(String email, String name, String password, int phone, Date fecharegistro, Rol rol, Provincias provincia) {
         this.email = email;
         this.name = name;
@@ -30,7 +50,60 @@ public class Usuario {
         this.provincia = provincia;
     }
 
-    public Usuario() {
+    public Usuario(Long ID, String email, String name, String password, int phone, Date fecharegistro, Rol rol, Provincias provincia, List<Trabajo> trabajosCliente, List<Trabajo> trabajosProveedor, List<Calificacion> calificacionesEmitidas, List<Calificacion> calificacionesRecibidas, String direccion) {
+        this.ID = ID;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.fecharegistro = fecharegistro;
+        this.rol = rol;
+        this.provincia = provincia;
+        this.trabajosCliente = trabajosCliente;
+        this.trabajosProveedor = trabajosProveedor;
+        this.calificacionesEmitidas = calificacionesEmitidas;
+        this.calificacionesRecibidas = calificacionesRecibidas;
+        this.direccion = direccion;
+    }
+
+    public List<Trabajo> getTrabajosCliente() {
+        return trabajosCliente;
+    }
+
+    public void setTrabajosCliente(List<Trabajo> trabajosCliente) {
+        this.trabajosCliente = trabajosCliente;
+    }
+
+    public List<Trabajo> getTrabajosProveedor() {
+        return trabajosProveedor;
+    }
+
+    public void setTrabajosProveedor(List<Trabajo> trabajosProveedor) {
+        this.trabajosProveedor = trabajosProveedor;
+    }
+
+    public List<Calificacion> getCalificacionesEmitidas() {
+        return calificacionesEmitidas;
+    }
+
+    public void setCalificacionesEmitidas(List<Calificacion> calificacionesEmitidas) {
+        this.calificacionesEmitidas = calificacionesEmitidas;
+    }
+
+    public List<Calificacion> getCalificacionesRecibidas() {
+        return calificacionesRecibidas;
+    }
+
+    public void setCalificacionesRecibidas(List<Calificacion> calificacionesRecibidas) {
+        this.calificacionesRecibidas = calificacionesRecibidas;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 
     public String getEmail() {
