5a648515a7c2ed4f8fa561eb32803e57d768d23d
xGaby <gabrielamar26@yahoo.es>
2023-09-14 20:14:44 -0500

    modificacion usuario repositorio: busqueda todos los clientes

--- src/main/java/com/equipoa/servicewebapp/Repositorios/UsuarioRepositorio.java
+++ src/main/java/com/equipoa/servicewebapp/Repositorios/UsuarioRepositorio.java
@@ -1,6 +1,8 @@
 package com.equipoa.servicewebapp.Repositorios;
 
 import com.equipoa.servicewebapp.Entidades.Usuario;
+import com.equipoa.servicewebapp.Enum.Rol;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,6 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
 
     @Query("SELECT u FROM Usuario u WHERE u.phone = :phone")
     public Usuario buscarPorTelefono(@Param("phone") int phone );
+    
+    List<Usuario> findAllByRol(Rol rol);
 }
