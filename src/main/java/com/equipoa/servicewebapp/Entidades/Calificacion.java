37ab72b7539e55b8081f365727d8408348982eaf
xGaby <gabrielamar26@yahoo.es>
2023-09-14 20:12:58 -0500

    Entidad Calificacion

--- src/main/java/com/equipoa/servicewebapp/Entidades/Calificacion.java
+++ src/main/java/com/equipoa/servicewebapp/Entidades/Calificacion.java
@@ -4,10 +4,44 @@
  */
 package com.equipoa.servicewebapp.Entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author Dell
  */
+@Entity
 public class Calificacion {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int puntuacion;
+    private String comentario;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_emisor_id")
+    private Usuario clienteEmisor; // el cliente que deja la calificación
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor_receptor_id")
+    private Usuario proveedorReceptor; // el proveedor que recibe la calificación
+
+    public Calificacion() {
+    }
+
+    public Calificacion(Long id, int puntuacion, String comentario, Usuario clienteEmisor, Usuario proveedorReceptor) {
+        this.id = id;
+        this.puntuacion = puntuacion;
+        this.comentario = comentario;
+        this.clienteEmisor = clienteEmisor;
+        this.proveedorReceptor = proveedorReceptor;
+    }
+
 }
