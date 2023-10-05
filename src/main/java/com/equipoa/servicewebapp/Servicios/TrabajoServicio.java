6881ced805fa4ea411911bb995d796113f52e2e5
akilacchi <67886272+akilacchi@users.noreply.github.com>
2023-10-05 12:58:21 -0300

    push con modificacion a perfil y modificar ocupacion restaurado

--- src/main/java/com/equipoa/servicewebapp/Servicios/TrabajoServicio.java
+++ src/main/java/com/equipoa/servicewebapp/Servicios/TrabajoServicio.java
@@ -83,7 +83,7 @@ public class TrabajoServicio {
             trabajo.setEstado(estado);
 
 //              Creacion de notificacion automatica
-            String mensaje = ("Solicitud de trabajo recivida por cliente " + logueado.getName() + " con el mensaje:\n" + descripcion);
+            String mensaje = ("Solicitud de trabajo recivida. Enviada por cliente: " + logueado.getName() + " con el mensaje: \n" + descripcion +" con fecha para: "+ trabajo.getFechaInicio());
             notificacionServicio.crearNotificacion(mensaje, logueado, idProveedor);
 //              Notificacion enviada
 
